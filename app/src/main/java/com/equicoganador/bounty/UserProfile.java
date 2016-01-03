27ddb50cd954869449db607d5a30242cb61e28c2
Alejandro Cuenca <alexcuenca4@gmail.com>
2016-01-03 03:10:45 -0600

    Commit for merge

--- app/src/main/java/com/equicoganador/bounty/UserProfile.java
+++ app/src/main/java/com/equicoganador/bounty/UserProfile.java
@@ -1,25 +1,32 @@
 package com.equicoganador.bounty;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+
+import butterknife.Bind;
 
 public class UserProfile extends Activity {
 
-
-
+    @Bind(R.id.user_image)
+    ImageView userImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
 
-        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
-        recList.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
+        recList.setHasFixedSize(true);
         recList.setLayoutManager(llm);
+
+
 
     }
 }
