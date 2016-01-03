3ff5e0094433976b582e6710c8407a32f7f47786
Rafa Miranda <rafayotuel@gmail.com>
2016-01-03 12:47:00 -0600

    Trophies room almost complete

--- app/src/main/java/com/equicoganador/bounty/TrophiesActivity.java
+++ app/src/main/java/com/equicoganador/bounty/TrophiesActivity.java
@@ -31,41 +31,87 @@ public class TrophiesActivity extends AppCompatActivity {
         ImageView imageView9 = (ImageView) findViewById(R.id.nineth_image);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView2);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView3);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView4);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView5);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView6);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView7);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView8);
 
         Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png")
+                .load("http://i.imgur.com/kPhqctc.png")
                 .into(imageView9);
 
+        ImageView monster = (ImageView) findViewById(R.id.monster1);
+        ImageView monster2 = (ImageView) findViewById(R.id.monster2);
+        ImageView monster3 = (ImageView) findViewById(R.id.monster3);
+        ImageView monster4 = (ImageView) findViewById(R.id.monster4);
+        ImageView monster5 = (ImageView) findViewById(R.id.monster5);
+        ImageView monster6 = (ImageView) findViewById(R.id.monster6);
+        ImageView monster7 = (ImageView) findViewById(R.id.monster7);
+        ImageView monster8 = (ImageView) findViewById(R.id.monster8);
+        ImageView monster9 = (ImageView) findViewById(R.id.monster9);
+
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster2);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster3);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster4);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster5);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster6);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster7);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster8);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/imd4cMV.png")
+                .into(monster9);
 
     }
 
